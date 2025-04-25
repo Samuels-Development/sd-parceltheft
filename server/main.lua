2ddc9813cba1aeb842ecae226b0bca8d142e3a72
Samuel Nicol <samnicol888@gmail.com>
2025-04-25 18:21:48 +0200

    fix/tweak?: pass variables to locale(s) correctly

--- server/main.lua
+++ server/main.lua
@@ -1,57 +1,61 @@
+-- server/main.lua
 local locale = SD.Locale.T
 local takenProps = {}
 
+-- When player uses a “parcel” item, tell their client to open the box
 SD.Inventory.RegisterUsableItem('parcel', function(source)
     TriggerClientEvent('sd-parceltheft:client:openBox', source)
 end)
 
+-- Return every package location plus whether it’s been taken
 SD.Callback.Register('sd-parceltheft:server:GetLocations', function(source)
     local locations = Config.Locations
-
     for k, location in pairs(locations) do
         location.taken = takenProps[k] or false
     end
-
     return locations
 end)
 
-local TakePackage = function(src, propId)
-    local playerCoords = GetEntityCoords(GetPlayerPed(src))
-    local propLocation = Config.Locations[propId].coords
-    local allowedRange = 5.0
-    local distance = #(playerCoords - propLocation)
+-- Attempt to pick up a package
+local function TakePackage(src, propId)
+    local playerCoords  = GetEntityCoords(GetPlayerPed(src))
+    local propLocation  = Config.Locations[propId].coords
+    local allowedRange  = 5.0
+    local distance      = #(playerCoords - propLocation)
 
     if takenProps[propId] then
-        print(locale('prints.package_already_taken', src))
+        print(locale('prints.package_already_taken', { src }))
         DropPlayer(src, locale('prints.package_already_taken_drop_reason'))
         return
     end
 
     if distance > allowedRange then
-        print(locale('prints.out_of_range', src, propId))
+        print(locale('prints.out_of_range', { src, propId }))
         DropPlayer(src, locale('prints.out_of_range_drop_reason'))
         return
     end
 
+    -- give them a parcel to carry
     SD.Inventory.AddItem(src, 'parcel', 1)
 
     takenProps[propId] = true
     TriggerClientEvent('sd-parceltheft:client:RemoveProp', -1, propId)
 end
 
-local GetRandomLoot = function()
+-- Randomly select loot without mutating the master table
+local function GetRandomLoot()
     local selectedItems = {}
-    local availableItems = Config.Loot.items
+    local availableItems = {}
+    for i, loot in ipairs(Config.Loot.items) do
+        availableItems[i] = loot
+    end
 
     for i = 1, Config.Loot.amount do
         local selectedKey = SD.Math.WeightedChance(availableItems)
-        
         if selectedKey then
-            local loot = availableItems[selectedKey]
+            local loot   = availableItems[selectedKey]
             local amount = math.random(loot.minAmount, loot.maxAmount)
-            
             table.insert(selectedItems, { item = loot.item, amount = amount })
-
             table.remove(availableItems, selectedKey)
         end
     end
@@ -59,15 +63,13 @@ local GetRandomLoot = function()
     return selectedItems
 end
 
+-- When client finishes “opening” the box, delete the parcel and give loot
 RegisterNetEvent('sd-parceltheft:server:deleteParcel', function()
     local src = source
-
     if not (SD.Inventory.HasItem(src, 'parcel') > 0) then return end
 
     SD.Inventory.RemoveItem(src, 'parcel', 1)
-
     local loot = GetRandomLoot()
-
     if loot and #loot > 0 then
         for _, item in ipairs(loot) do
             SD.Inventory.AddItem(src, item.item, item.amount)
@@ -75,18 +77,19 @@ RegisterNetEvent('sd-parceltheft:server:deleteParcel', function()
     end
 end)
 
+-- Mark a prop taken, then reset after cooldown
 RegisterNetEvent('sd-parceltheft:server:MarkPropTaken', function(propId)
-    local src = source
+    local src           = source
     local countdownTime = Config.PropCooldown
 
     TakePackage(src, propId)
 
     CreateThread(function()
         Wait(countdownTime * 1000)
-
         takenProps[propId] = nil
         TriggerClientEvent('sd-parceltheft:client:ResetProp', -1, propId)
     end)
 end)
 
-SD.CheckVersion('Samuels-Development/sd-parceltheft') -- Check version of specified resource
+-- version check
+SD.CheckVersion('Samuels-Development/sd-parceltheft')
